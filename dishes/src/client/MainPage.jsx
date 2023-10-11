fc294199eb254269b3f0b6bf3ea149e44c2148ec
Michael <egg-meister@hotmail.com>
2023-10-11 21:07:11 +0200

    Created basic main page

--- dishes/src/client/MainPage.jsx
+++ dishes/src/client/MainPage.jsx
@@ -1,6 +1,35 @@
+import getTasks from '@wasp/queries/getTasks'
+import { useQuery } from '@wasp/queries'
+
 const MainPage = () => {
+  const { data: tasks, isLoading, error } = useQuery(getTasks)
   return (
-    <div>Hello World!</div>
+    <div>
+      {tasks && <TasksList tasks={tasks} />}
+      {isLoading && 'Loading...'}
+      {error && 'Error: '+error}
+    </div>
   )
 }
+
+const Task = ({ task }) => {
+  return(
+    <div>
+      <input type="checkbox" id={String(task.id)} checked={task.isDone} />
+      {task.description}
+    </div>
+  )
+}
+
+const TasksList = ({ tasks }) => {
+  if (!tasks?.length) return <div>No tasks.</div>
+  return (
+    <div>
+      {tasks.map((task, idx) => (
+        <Task task={task} key={idx} />
+      ))}
+    </div>
+  )
+}
+
 export default MainPage
